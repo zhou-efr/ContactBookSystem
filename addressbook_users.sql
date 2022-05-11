71284532a47794fb1b30f9d3d86f93ef93ea114b
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-05-12 01:21:36 +0800

    datasources

---
+++ addressbook_users.sql
@@ -0,0 +1,5 @@
+insert into addressbook.users (id, username, password, email, role, lastLogin)
+values  (1, 'zhou', 'd73147d23ad727ddc25044ff0f4b2773', 'killian.zhou@efrei.net', 'user', '2022-04-30'),
+        (2, 'panda', 'd73147d23ad727ddc25044ff0f4b2773', 'thepandagundam@gmail.com', 'admin', '2022-04-30'),
+        (5, 'ourspolaire', 'd73147d23ad727ddc25044ff0f4b2773', 'ours@panda.panda', 'user', '2022-05-11'),
+        (6, 'oursbrun', 'd73147d23ad727ddc25044ff0f4b2773', 'ours@panda.panda', 'user', '2022-04-29');
